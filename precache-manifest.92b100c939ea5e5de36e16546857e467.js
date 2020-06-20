551d9e5e9b6021c34ad54a15f8a5860e30e3ad59
Baraja Swargiary <barajaswargiary4444@gmail.com>
2020-06-20 10:39:18 +0530

    Updates

---
+++ precache-manifest.92b100c939ea5e5de36e16546857e467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225df6d7379633d74be32ce576d9a77f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ed24055f187a3cb66c71",
+    "url": "/monsters-rolodex/static/css/main.0d6239dd.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24055f187a3cb66c71",
+    "url": "/monsters-rolodex/static/js/main.6509ff9c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
